444bb5bd2b7ba85adba765b184ab6b30e6dd649c
Hector Andres Perez Villatoro <hectorandres.pv@gmail.com>
2021-01-24 08:45:10 -0800

    mending sql strings

--- tools/sqlStrings.js
+++ tools/sqlStrings.js
@@ -1,6 +1,6 @@
 e = module.exports;
 
-e.create_friend = `insert into friends (, , , ) values($1,$2,$3,$4) `;
-e.read_friend = `select * from friends where property = $1`;
-e.update_friend = `update set property = $1 where property2 = $2`;
-e.delete_friend = `delete from friends where property $1`;
+e.create_friend = `insert into friends (first_name, last_name, age , added) values($1,$2,$3,$4) `;
+e.read_friend = `select * from friends where first_name = $1`;
+e.update_friend = `update friends set age = $1 where first_name = $2`;
+e.delete_friend = `delete from friends where first_name = $1`;
